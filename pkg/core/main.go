cf53cd1bd90c87eae094c66dc4381e4705a6b075
Muhammadnumon <mnumojon@mail.ru>
2020-02-14 02:06:36 +0300

    a

--- pkg/core/main.go
+++ pkg/core/main.go
@@ -9,6 +9,27 @@ type QueryError struct {
 Query string
 Err error
 }
+type DbError struct {
+	Err error
+}
+
+func (receiver *DbError) Error() string {
+	return fmt.Sprintf("cant handle db operatin:%v",receiver.Err.Error() )
+}
+
+func dbError(err error) *DbError {
+	return &DbError{Err: err}
+}
+type BankMachine struct{
+	Id int64
+	Name string
+}
+type BankAccounts struct {
+	Id int64
+	Name string
+	BankAccount int64
+	Balance int64
+}
 
 func (receiver QueryError) Error() string {
 	return fmt.Sprintf("can't execute query %s:%s",loginSQL,receiver.Err.Error())
@@ -19,7 +40,22 @@ var ErrInvalidPass=errors.New("Invalid password")
 func queryError(query string,err error) *QueryError{
 	return &QueryError{Query: query, Err: err}
 }
-func Login(login string,password int,db sql.DB)(bool,error){
+func Init(db *sql.DB) (err error) {
+	ddls := []string{clientsDDL,bankMachinesDDL}
+	for _, ddl := range ddls {
+		_, err = db.Exec(ddl)
+		if err != nil {
+			return err
+		}}
+	initialData:=[]string{clientsInitialData,bankMachinesInitialData}
+	for _,info:=range initialData{
+		_,err=db.Exec(info)
+		if err !=nil{
+			return err
+		}}
+	return nil
+}
+func Login(login string,password int,db *sql.DB)(bool,error){
 var dbLogin string
 var dbPassword int
 err:=db.QueryRow(
@@ -35,7 +71,54 @@ err:=db.QueryRow(
 	}
 	return true, nil
 }
+func Machine(db *sql.DB)(machines []BankMachine, err error){
+	rows,err:=db.Query(showBankMachine)
+	if err!=nil{
+		return nil,queryError(showBankMachine,err)
+	}
+defer func() {
+	if innerErr := rows.Close(); innerErr != nil {
+		machines, err = nil, dbError(innerErr)
+	}
+}()
+	for rows.Next(){
+		machine:=BankMachine{}
+		err=rows.Scan(&machine.Id,&machine.Name)
+		if err != nil {
+			return nil, dbError(err)
+		}
+		machines=append(machines,machine)
+	}
+	if rows.Err()!= nil{
+		return nil,dbError(rows.Err())
+	}
 
+	return machines, nil
+}
+func Account(db *sql.DB)(accounts []BankAccounts, err error){
+	rows,err:=db.Query(bankAccount)
+	if err!=nil{
+		return nil,queryError(bankAccount,err)
+	}
+	defer func() {
+		if innerErr := rows.Close(); innerErr != nil {
+			accounts, err = nil, dbError(innerErr)
+		}
+	}()
+	for rows.Next(){
+		account:=BankAccounts{}
+		err=rows.Scan(&account.Id,&account.Name,&account.BankAccount,&account.Balance)
+		if err != nil {
+			return nil, dbError(err)
+		}
+		accounts=append(accounts,account)
+	}
+	if rows.Err()!= nil{
+		return nil,dbError(rows.Err())
+	}
+
+	return accounts, nil
+}
 func main() {
 	
 }
--- pkg/core/sql.go
+++ pkg/core/sql.go
@@ -1,10 +1,35 @@
 package core
 
-const clientsDDL  =`create table if not exists clients(
+const clientsDDL = `create table if not exists clients(
     id integer primary key autoincrement ,
     name text not null,
     login text not null unique ,
     password integer not null ,
-    bankAccount integer not null 
-    )`
-const loginSQL=`select login,password from clients where login ?`
+    bankAccount integer not null ,
+    phoneNumber integer not null unique,
+    balance integer not null check(balance>=0)
+    );`
+const bankMachinesDDL = `
+CREATE TABLE IF NOT EXISTS bankMachine
+(
+    id    INTEGER PRIMARY KEY AUTOINCREMENT,
+    name  TEXT    NOT NULL UNIQUE,
+    street  TEXT    NOT NULL UNIQUE
+);`
+const servicesDDL=`create table if not exists services(
+id integer primary key autoincrement,
+name text not null,
+price integer not null check(price>0)
+);`
+const bankMachinesInitialData = `INSERT INTO bankMachine(id, name)
+VALUES (1, 'Alif bankMachine 1 ','Chapayev 24'),       
+       (2, 'Alif bankMachine 2 ','Nemat Karaboyev 44'),  
+       (3, 'Alif bankMachine 3 ','Yakkatut 26')
+ON CONFLICT DO NOTHING;`
+const clientsInitialData = `insert into clients
+values(1, 'Vasya', 'vasya', 1000, 21060001,934646999,3000),
+      (2, 'Petya', 'petya', 2000, 21060002,938520111,2500)
+       ON CONFLICT DO NOTHING;`
+const loginSQL = `select login,password from clients where login =?`
+const showBankMachine = `select id,name,street from bankMachine `
+const bankAccount = `select id,name,bankAccount,balance from clients where id=?`
